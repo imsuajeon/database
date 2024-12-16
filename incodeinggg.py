03d06390ba35ae3ac1d0512ab0f396c1b8b2414d
chaechae922 <cele9222@naver.com>
2024-12-16 14:25:47 +0900

    강의실 상태 조회_DB 관리 페이지

--- incodeinggg.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# CSV 파일 읽기 (원래 인코딩 설정)
-df = pd.read_csv("C:\Users\suaje\OneDrive\Desktop\편람.csv", encoding="CP949")  # 또는 'EUC-KR'
-
-# UTF-8로 저장
-df.to_csv("C:\Users\suaje\OneDrive\Desktop\편람.csv", encoding="utf-8", index=False)
